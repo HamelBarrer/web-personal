82f44549563e6aa47f2ab54605ce3722310fba7d
Hamel Barrero <hamel_-barrero@hotmail.com>
2020-06-07 22:44:29 -0500

    config design

--- profiles/views.py
+++ profiles/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
 from django.views.generic import View
+from django.contrib.auth.decorators import login_required
 
 from .models import Profile
 from .forms import ProfileForm
 
 
+@login_required(login_url='users:login')
 def profile_view(request):
     template_name = 'profiles/profil.html'
     user = request.user
     profile = Profile.objects.filter(user=user).first()
 
     return render(request, template_name, {
+        'user': user,
         'profile': profile,
     })
